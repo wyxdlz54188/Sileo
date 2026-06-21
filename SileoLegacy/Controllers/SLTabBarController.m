b8d549c965bee4a76cf5b0fff2263b38928c190e
wyxdlz54188 <monkeycode-ai@chaitin.com>
2026-06-21 18:22:00 +0000

    fix: restore updated existing controllers Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- SileoLegacy/Controllers/SLTabBarController.m
+++ SileoLegacy/Controllers/SLTabBarController.m
@@ -1,35 +1,36 @@
-#import "Controllers/SLAppDelegate.h"
 #import "Controllers/SLTabBarController.h"
-#import "Controllers/SLPackagesViewController.h"
+#import "Controllers/SLHomeViewController.h"
 #import "Controllers/SLSourcesViewController.h"
+#import "Controllers/SLPackagesViewController.h"
+#import "Controllers/SLSearchViewController.h"
 #import "Controllers/SLDownloadsViewController.h"
-
-@interface SLTabBarController ()
-
-@property (nonatomic, strong) SLPackagesViewController *packagesVC;
-@property (nonatomic, strong) SLSourcesViewController *sourcesVC;
-@property (nonatomic, strong) SLDownloadsViewController *downloadsVC;
-
-@end
 
 @implementation SLTabBarController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.packagesVC = [[SLPackagesViewController alloc] init];
-    UINavigationController *packagesNav = [[UINavigationController alloc] initWithRootViewController:self.packagesVC];
-    packagesNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Packages" image:[UIImage imageNamed:@"package"] tag:0];
-    
-    self.sourcesVC = [[SLSourcesViewController alloc] init];
-    UINavigationController *sourcesNav = [[UINavigationController alloc] initWithRootViewController:self.sourcesVC];
-    sourcesNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Sources" image:[UIImage imageNamed:@"source"] tag:1];
-    
-    self.downloadsVC = [[SLDownloadsViewController alloc] init];
-    UINavigationController *downloadsNav = [[UINavigationController alloc] initWithRootViewController:self.downloadsVC];
-    downloadsNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Downloads" image:[UIImage imageNamed:@"download"] tag:2];
-    
-    self.viewControllers = @[packagesNav, sourcesNav, downloadsNav];
+
+    SLHomeViewController *homeVC = [[SLHomeViewController alloc] init];
+    UINavigationController *homeNav = [[UINavigationController alloc] initWithRootViewController:homeVC];
+    homeNav.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemFeatured tag:0];
+
+    SLSourcesViewController *sourcesVC = [[SLSourcesViewController alloc] init];
+    UINavigationController *sourcesNav = [[UINavigationController alloc] initWithRootViewController:sourcesVC];
+    sourcesNav.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemHistory tag:1];
+
+    SLPackagesViewController *packagesVC = [[SLPackagesViewController alloc] init];
+    UINavigationController *packagesNav = [[UINavigationController alloc] initWithRootViewController:packagesVC];
+    packagesNav.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemMostViewed tag:2];
+
+    SLSearchViewController *searchVC = [[SLSearchViewController alloc] init];
+    UINavigationController *searchNav = [[UINavigationController alloc] initWithRootViewController:searchVC];
+    searchNav.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemSearch tag:3];
+
+    SLDownloadsViewController *downloadsVC = [[SLDownloadsViewController alloc] init];
+    UINavigationController *downloadsNav = [[UINavigationController alloc] initWithRootViewController:downloadsVC];
+    downloadsNav.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemDownloads tag:4];
+
+    self.viewControllers = @[homeNav, sourcesNav, packagesNav, searchNav, downloadsNav];
     self.selectedIndex = 0;
 }
 
